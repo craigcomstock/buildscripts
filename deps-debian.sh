f13a793e9778dbc54657a72af612209d273e475f
Craig Comstock <craig.comstock@northern.tech>
2022-07-19 13:29:07 -0500

    initial work, nice to include code for references!

---
+++ deps-debian.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+echo "Installing build dependencies"
+apt update -y
+apt upgrade -y
+apt install -y \
+  autoconf \
+  automake \
+  m4 \
+  make \
+  bison \
+  flex \
+  binutils \
+  libtool \
+  gcc \
+  g++ \
+  libc-dev \
+  libpam0g-dev \
+  python3 \
+  psmisc \
+  libtokyocabinet-dev \
+  libssl-dev \
+  libpcre3-dev \
+  default-jre-headless \
+  build-essential \
+  fakeroot \
+  ntp \
+  dpkg-dev \
+  debhelper \
+  pkg-config \
+  nfs-common \
+  sudo \
+  apt-utils \
+  wget \
+  libncurses5 \
+  rsync \
+  libexpat1-dev \
+  libexpat1
+apt purge -y \
+  emacs \
+  emacs24 \
+  libltdl-dev \
+  libltdl7 \
+apt install -y curl # as it seems wget is broken maybe? or maybe just on ubuntu, for install-dependencies
--- i
+++ i
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 set -i
 set -x
-asciidoctor README.adoc
-exit 0
+#asciidoctor README.adoc
+#exit 0
 command -v parallel >/dev/null || brew install parallel
 command -v fswatch >/dev/null || brew install fswatch
 #command -v asciidoctor >/dev/null || brew install asciidoctor
 command -v md2html >/dev/null || brew install md4c
 #asciidoctor *.adoc
-fswatch *.md | parallel "md2html {} > {}.html"
+fswatch *.adoc | parallel asciidoctor {}
 # then manually refresh in browser window already open
 # TODO add bits like hugo to auto-refresh on file change?
 # https://github.com/livereload/livereload-js is what hugo uses. so need a server. Make a small one in C! :O
