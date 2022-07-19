ba32a21817c5740aa7ea74b6b6d84e362bcc9cbd
Craig Comstock <craig.comstock@northern.tech>
2022-07-19 13:31:32 -0500

    fixup

--- builder.sh
+++ builder.sh
@@ -11,21 +11,21 @@ BUILDER_NAME="ubuntu-builder"
 buildah rm "$BUILDER_NAME" || true # just in case, delete it first
 
 #builder=$(buildah --name "$BUILDER_NAME" from ubuntu)
-#buildah copy $builder build-deps.sh /cfe/
-#buildah run $builder -- /bin/bash /cfe/build-deps.sh
+#buildah copy $builder deps-debian.sh /cfe/
+#buildah run $builder -- /bin/bash /cfe/deps-debian.sh
 #buildah commit $builder builder:build-deps-installed
 
 #git clone --recursive --depth 1 https://github.com/cfengine/core || true
 #git clone --depth 1 https://github.com/cfengine/buildscripts || true
 #buildah copy $builder core /cfe/core
 #buildah copy $builder buildscripts /cfe/buildscripts
-#buildah copy $builder build-env.sh /cfe/
+#buildah copy $builder env-community-agent.sh /cfe/
 #buildah commit $builder builder:cfengine-sources-copied
 
 builder=$(buildah --name "$BUILDER_NAME" from "$BUILD_IMAGE")
 echo "builder is $builder"
 # run this once... somethow :)
-#cat build-env.sh | buildah run $builder -- tee -a /root/.bashrc
+#cat env-community-agent.sh | buildah run $builder -- tee -a /root/.bashrc
 buildan run $builder -- bash -i /cfe/buildscripts/build-scripts/autogen
 # maybe better to put everything at the root? default root for buildah/container?
 buildah run $builder -- bash -i /cfe/buildscripts/build-scripts/clean-buildmachine
