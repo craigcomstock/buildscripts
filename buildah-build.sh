db3f2f060a6c61586ae6689ffb259c4b43de2ff7
Craig Comstock <craig.comstock@northern.tech>
2022-07-20 14:17:51 -0500

    Craigs-MBP.lan

--- buildah-build.sh
+++ buildah-build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 set -x
+# TODO parameterize this so we can just
+# buildah from whatever(ubuntu, debian, rhel, centos, alpine, freebsd)
+# and have the rest of the business "just work"
 PLATFORM=x86_64_linux_ubuntu_20
 # BASE_IMAGE is the operating system + build dependency packages installed
 # TODO refresh this periodically with apt-get update apt-get upgrade apt-get clean apt-get autoclean apt-get autoremove
@@ -13,6 +16,7 @@ buildah rm "$BUILDER_NAME"
 
 if ! buildah images --quiet "$BASE_IMAGE"; then
   tmp=$(buildah from ubuntu)
+  # TODO make more different scripts or even policy to install deps on various platforms
   buildah copy $tmp deps-debian.sh
   time buildah run $tmp -- bash -x deps-debian.sh
   time buildah commit $tmp $BASE_IMAGE
@@ -20,6 +24,7 @@ if ! buildah images --quiet "$BASE_IMAGE"; then
 fi
 
 builder=$(buildah --name "$BUILDER_NAME" from "$BASE_IMAGE")
+# TODO change name of this script and add if/elses for enterprise agent and enterprise hub
 time buildah copy $builder $NTECH_ROOT/core /cfe/core
 time buildah copy $builder $NTECH_ROOT/buildscripts /cfe/buildscripts
 time buildah copy $builder $NTECH_ROOT/masterfiles /cfe/masterfiles
