ade34cf052ed405ade49a98d2940fd3945a86bae
Craig Comstock <craig.comstock@northern.tech>
2025-08-08 16:43:20 -0500

    sample env script to source for building locally

---
+++ ci/env.sh
@@ -0,0 +1,8 @@
+# sample env vars to source
+export PROJECT=nova
+export NO_CONFIGURE=1
+export BUILD_TYPE=DEBUG
+export ESCAPETEST=yes
+export EXPLICIT_ROLE=hub
+export TEST_MACHINE=chroot
+export JOB_BASE_NAME=PACKAGES_HUB_x86_64_linux_ubuntu_20
